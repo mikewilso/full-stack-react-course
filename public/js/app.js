75ce0f659ce2cb1ce472f98dd7b88eaa8f24a2bb
Michael Wilson <miwilson@sovrn.com>
2019-04-15 15:09:45 -0600

    functonal add timer button to create new timers

--- public/js/app.js
+++ public/js/app.js
@@ -16,6 +16,15 @@ class TimersDashboard extends React.Component {
       }, 
     ],
   };
+  handleCreateFormSubmit = (timer) => {
+    this.createTimer(timer);
+  };
+  createTimer = (timer) => {
+    const t = helpers.newTimer(timer);
+    this.setState({
+      timers: this.state.timers.concat(t),
+    });
+  };
 
   render() {
     return (
@@ -25,7 +34,7 @@ class TimersDashboard extends React.Component {
                 timers={this.state.timers}
               />
               <ToggleableTimerForm
-                isOpen={false} 
+                onFormSubmit={this.handleCreateFormSubmit}
               />
             </div>
           </div>
@@ -119,25 +128,54 @@ class TimerForm extends React.Component {
     title: this.props.title || '',
     project: this.props.project || '',
   };
+  handleTitleChange = (e) => {
+    this.setState({ title: e.target.value });
+  }
+  handleProjectChange = (e) => {
+    this.setState({ project: e.target.value });
+  }
+  handleSubmit = () => {
+    this.props.onFormSubmit({
+      id: this.props.id,
+      title: this.state.title,
+      project: this.state.project,
+    }); 
+  };
+
   render() 
   {
-    const submitText = this.props.title ? 'Update' : 'Create'; return (
+    const submitText = this.props.id ? 'Update' : 'Create'; 
+    return (
       <div className='ui centered card'>
         <div className='content'>
           <div className='ui form'>
             <div className='field'>
               <label>Title</label>
-              <input type='text' defaultValue={this.props.title} /> 
+              <input 
+                type='text' 
+                value={this.state.title} 
+                onChange={this.handleTitleChange}
+              /> 
             </div>
               <div className='field'>
                 <label>Project</label>
-                <input type='text' defaultValue={this.props.project} />
+                <input 
+                  type='text' 
+                  value={this.state.project}
+                  onChange={this.handleProjectChange}
+                />
               </div>
             <div className='ui two bottom attached buttons'>
-              <button className='ui basic blue button'>
+              <button 
+                className='ui basic blue button'
+                onClick={this.handleSubmit}
+              >
                 {submitText}
               </button>
-              <button className='ui basic red button'>
+              <button 
+                className='ui basic red button'
+                onClick={this.props.onFormClose}
+              >
                   Cancel
               </button>
             </div>
@@ -155,17 +193,27 @@ class ToggleableTimerForm extends React.Component {
   handleFormOpen = () => {
     this.setState({ isOpen: true});
   }
+  handleFormClose = () => {
+    this.setState({ isOpen: false });
+  };
+  handleFormSubmit = (timer) => {
+    this.props.onFormSubmit(timer);
+    this.setState({ isOpen: false});
+  }
   render() {
     if (this.state.isOpen) { 
       return (
-          <TimerForm />
+          <TimerForm 
+            onFormSubmit={this.handleFormSubmit}
+            onFormClose={this.handleFormClose}
+          />
       );
   } else { 
     return (
       <div className='ui basic content center aligned segment'>
           <button 
             className='ui basic button icon'
-            onClick={this.handFormOpen}
+            onClick={this.handleFormOpen}
           >
             <i className='plus icon' />
           </button>
